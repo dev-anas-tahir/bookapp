86fd75fadda268766d4d13c9b87519492138d8f3
Anas Waleed Tahir <116335388+dev-anas-tahir@users.noreply.github.com>
2026-02-17 03:21:26 +0500

    Update configuration and environment handling: enhance .env file patterns, add Postman config, and introduce Justfile for server management; refactor database connection settings to use environment variables.

--- piccolo_conf.py
+++ piccolo_conf.py
@@ -1,11 +1,15 @@
 from piccolo.engine import PostgresEngine
+from environs import Env
+
+env = Env()
+env.read_env(".env.dev")
 
 DB = PostgresEngine(
     config={
-        "user": "piccolo",
-        "password": "piccolo",
-        "host": "localhost",
-        "database": "bookapp",
-        "port": 5432,
+        "user": env("user"),
+        "password": env("password"),
+        "host": env("host"),
+        "database": env("database"),
+        "port": env.int("port"),
     }
 )
